476c031072db0486a04ddc5351deae88f079f5a0
fsalcedo55 <fsalcedo55@hotmail.com>
2024-02-24 10:13:22 -0500

    functions

---
+++ 02basics/myfunctions.ts
@@ -0,0 +1,15 @@
+function addTwo(num: number) {
+  return num + 2
+}
+
+function getUpper(val: string) {
+  return val.toUpperCase()
+}
+
+function signUpUser(name: string, email: string, isPaid: string) {}
+
+addTwo(5)
+getUpper("hello")
+signUpUser("John", "hello@hello.com", "true")
+
+export {}
